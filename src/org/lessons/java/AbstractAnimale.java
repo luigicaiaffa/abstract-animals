7b7c56bab37ae59ebdda82df39e9a2f3e410e22a
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-11 12:17:28 +0100

    config class AbstractAnimale

--- src/org/lessons/java/AbstractAnimale.java
+++ src/org/lessons/java/AbstractAnimale.java
@@ -1,5 +1,47 @@
 package org.lessons.java;
 
 public abstract class AbstractAnimale {
-    
+
+    // # Variabili di istanza
+    protected String name;
+    protected int age;
+
+    // # Costruttori
+    public AbstractAnimale(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public AbstractAnimale() {
+        this.name = "Unnamed";
+        this.age = 0;
+    }
+
+    // # Metodi
+
+    // * Getter e Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    // * Metodi aggiuntivi
+    public void dormi() {
+        System.out.println("Zzzz...");
+    }
+
+    public abstract void verso();
+
+    public abstract void mangia();
 }
